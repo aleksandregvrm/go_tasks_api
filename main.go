fdf81e34fb700c6e8184c53d8b797a7334cfa195
aleksandre <aleksandregvrm@gmail.com>
2024-11-04 18:43:25 +0400

    structure for users added

--- main.go
+++ main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"net/http"
-
-	events "example.com/api/models"
+	"example.com/rest-api/db"
+	"example.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
-	server.Run(":8080")
-
-}
-
-func getEvents(context *gin.Context) {
-	events := events.GetAllEvents()
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event events.Event
-	err := context.ShouldBindJSON(&event)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"err": "Invalid data sent"})
-	}
-
-	event.ID = 1
-	event.UserId = 1
-	event.Save()
-	context.JSON(http.StatusCreated, gin.H{"msg": "Event was created", "event": event})
+	server.Run(":8080") // localhost:8080
 }
